b3b2380a5ff8db09c1a7dcbe8c313224a28dfbcb
Alejandro <diaza913@gmail.com>
2020-06-24 22:03:49 -0600

    Email class and function added an working 	new file:   Logic/MailMan.cs

---
+++ Logic/MailMan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoTEC_Server.Logic
+{
+    public class MailMan
+    {
+
+        public static void Email(string msg, string sub)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("diaza913@gmail.com");
+                message.To.Add(new MailAddress("diaza913@gmail.com"));
+                message.Subject = sub;
+                message.IsBodyHtml = false; //to make message body as html  
+                message.Body = msg;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("diaza913@gmail.com", "@cer_2599");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) { }
+        }
+
+    }
+}
